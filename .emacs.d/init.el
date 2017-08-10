aee640631cdfaad76519bbd35d1644de368cd57a
woocat <563268560@qq.com>
2017-08-10 23:27:57 +0800

    add org-mode config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,6 +25,7 @@
  '(flycheck-disabled-checkers
    (quote
     (go-gofmt go-vet go-test go-errcheck go-unconvert go-megacheck)))
+ '(org-log-into-drawer t)
  '(package-selected-packages
    (quote
     (evil-magit magit ggtags evil-leader ace-window avy company-go go-eldoc go-mode powerline solarized-theme hungry-delete popwin exec-path-from-shell smartparens counsel-projectile counsel evil org org-bullets company use-package))))
@@ -33,4 +34,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-checkbox ((t (:background "#fdf6e3" :foreground "#657b83" :box nil)))))
