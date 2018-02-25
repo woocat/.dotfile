f97fcd705b0610e914c462d313432b3b65824cf6
woocat <catbrain@163.com>
2018-02-26 07:32:16 +0800

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -39,4 +39,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((nil (:background "#49483E" :foreground "#F8F8F0" :inverse-video nil :box (:line-width 1 :color "#64645E" :style unspecified)))))
+ '(mode-line-highlight ((t (:box nil)))))
